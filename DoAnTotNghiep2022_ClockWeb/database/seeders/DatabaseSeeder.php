4578c803ec67a84a5eabc96e50fef3a2dad8fa6a
vutran3001 <62737402+vutran3001@users.noreply.github.com>
2022-06-09 09:34:53 +0700

    vutq/edit_slide

--- DoAnTotNghiep2022_ClockWeb/database/seeders/DatabaseSeeder.php
+++ DoAnTotNghiep2022_ClockWeb/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            LoaiSanPhamSeeder::class,
+            NhaCungCapSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
     }
